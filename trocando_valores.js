719e33d7b81e5c13382bced1c3a428082105c919
GabiDev <115039314+Gabigurgelc@users.noreply.github.com>
2024-01-29 18:50:07 -0300

    Add files via upload

---
+++ trocando_valores.js
@@ -0,0 +1,20 @@
+var tecnologias = [
+    "JavaScript",
+    "PHP",
+    "Python",
+    "Java",
+    "Delphi"
+];
+
+var novas_tecnologias = [
+    "TypeScript",
+    "Dart"
+];
+
+//trocando o valores de um Array:
+tecnologias[2] = novas_tecnologias[1]
+tecnologias[4] = novas_tecnologias[0];
+
+
+//verificando se a troca foi feita::
+console.log(tecnologias);
